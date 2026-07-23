55d8658b1df2dbc80259de61f84f8d811e6d16ec
boxeyed <keertr228@gmail.com>
2026-07-22 18:56:45 -0600

    removed con.close from setupdb since it was causing a closed connection error

--- manage_db.py
+++ manage_db.py
@@ -45,7 +45,6 @@ def setup_db(con: sqlite3.Connection):
 
 
     con.commit()
-    con.close()
 
 def wipe_db(con: sqlite3.Connection):
     verification = input("This will wipe all data from the database. Type 'YES' to confirm: ").strip
