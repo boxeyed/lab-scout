6272fef6ca1dd07ef755d1fc0ff8d370d1ea93ea
boxeyed <keertr228@gmail.com>
2026-07-23 18:22:06 -0600

    extra comma

--- manage_db.py
+++ manage_db.py
@@ -100,7 +100,7 @@ def migrate(con: sqlite3.Connection, csv_path=CSV_PATH):
         for topic_name in row["Topics"].split(","):
             topic_name = topic_name.strip()
             if topic_name not in topic_ids:
-                cursor = con.execute("INSERT INTO topics (name) VALUES (?)", (topic_name,))
+                cursor = con.execute("INSERT INTO topics (name) VALUES (?)", (topic_name)) # the internal cursor return
                 topic_ids[topic_name] = cursor.lastrowid # for lab_x_topics
 
             # to 'lab_x_topics'
