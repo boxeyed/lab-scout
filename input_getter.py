51098df2c96268a7f51ca94817f830e1f6f33d32
boxeyed <keertr228@gmail.com>
2026-08-09 19:47:29 -0600

    added character limit to input getter (64)

--- input_getter.py
+++ input_getter.py
@@ -1,4 +1,5 @@
 # Input prompter separate for the purpose of isoloating test cases.
+CHAR_LIMIT = 64
 
 def scout_lab_input():
     return input("Enter topic(s) to scout labs for.\n-> ")
@@ -16,5 +17,29 @@ def add_lab_input():
     contact_email = input("Contact Email (optional): ").strip()
     recruit_status = input("Recruiting Status (optional): ").strip()
     website = input("Website (optional): ").strip()
+
+    if not contact_name:
+        contact_name = "Unknown"
+    if len(contact_name) > CHAR_LIMIT:
+        print("Character limit of 64 surpassed. Try again.")
+        return
+    
+    if not contact_email:
+        contact_email = "Unknown"
+    if len(contact_email) > CHAR_LIMIT:
+        print("Character limit of 64 surpassed. Try again.")
+        return
+    
+    if not recruit_status:
+        recruit_status = "Unknown"
+    if len(recruit_status) > CHAR_LIMIT:
+        print("Character limit of 64 surpassed. Try again.")
+        return
+    
+    if not website:
+        website = "Unknown"
+    if len(website) > CHAR_LIMIT:
+        print("Character limit of 64 surpassed. Try again.")
+        return
 
     return title, topics, contact_name, contact_email, recruit_status, website
